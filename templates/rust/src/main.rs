79d92873247bee1ac7ddb56366aed05c901dec46
Sam Walker <swalker@pop-os.localdomain>
2024-12-03 14:08:54 +1300

    Create rust template

---
+++ templates/rust/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("");
+    println!("--- Day DAY: NAME ---");
+    println!("Part one: {}", part_one("".to_string()));
+    println!("Part two: {}", part_two("".to_string()));
+}
+
+fn part_one(_input: String) -> usize {
+    return 0;
+}
+
+fn part_two(_input: String) -> usize {
+    return 0;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one("".to_string()), 1);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two("".to_string()), 1);
+    }
+}
