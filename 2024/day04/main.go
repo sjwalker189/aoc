c11ee27aebb4642d34d9b33af9f8a9065d1a2f51
Sam Walker <sjwalker189@gmail.com>
2024-12-07 15:41:30 +1300

    2024 Day 4 part two

--- 2024/day04/main.go
+++ 2024/day04/main.go
@@ -13,21 +13,73 @@ func main() {
 		os.Exit(1)
 	}
 
-	p1, p2 := Solve(string(input))
+	p1 := partOne(string(input))
+	p2 := partTwo(string(input))
 
 	fmt.Println("--- Day 04: Ceres Search ---")
 	fmt.Printf("Part One: %d\n", p1)
 	fmt.Printf("Part Two: %d\n", p2)
-
 }
 
-func Solve(input string) (int, int) {
+func partOne(input string) int {
 	grid := NewGrid(input)
-	return partOne(&grid), 0
+	word := []rune("XMAS")
+
+	var matches int
+
+	for x := range grid.Cols {
+		for y := range grid.Rows {
+			matches += grid.Search(x, y, DirNorth, word)
+			matches += grid.Search(x, y, DirNorthEast, word)
+			matches += grid.Search(x, y, DirEast, word)
+			matches += grid.Search(x, y, DirSouthEast, word)
+			matches += grid.Search(x, y, DirSouth, word)
+			matches += grid.Search(x, y, DirSouthWest, word)
+			matches += grid.Search(x, y, DirWest, word)
+			matches += grid.Search(x, y, DirNorthWest, word)
+		}
+	}
+
+	return matches
 }
 
-func partOne(grid *Grid) int {
-	return grid.CountWord([]rune("XMAS"))
+func partTwo(input string) int {
+	grid := NewGrid(input)
+
+	// Define each rune as a variable to avoid allocations
+	M := 'M'
+	A := 'A'
+	S := 'S'
+
+	var matches int
+
+	for x := range grid.Cols {
+		for y := range grid.Rows {
+
+			// We only need to search when we enounter the middle character
+			if grid.RuneAt(x, y) != A {
+				continue
+			}
+
+			matchLeft := false
+			if (grid.RuneAt(x-1, y-1) == M && grid.RuneAt(x+1, y+1) == S) ||
+				(grid.RuneAt(x-1, y-1) == S && grid.RuneAt(x+1, y+1) == M) {
+				matchLeft = true
+			}
+
+			matchRight := false
+			if (grid.RuneAt(x+1, y-1) == M && grid.RuneAt(x-1, y+1) == S) ||
+				(grid.RuneAt(x+1, y-1) == S && grid.RuneAt(x-1, y+1) == M) {
+				matchRight = true
+			}
+
+			if matchLeft && matchRight {
+				matches++
+			}
+		}
+	}
+
+	return matches
 }
 
 type Direction int
@@ -49,7 +101,7 @@ type Grid struct {
 	Cols  int
 }
 
-func (grid *Grid) search(x int, y int, dir Direction, word []rune) int {
+func (grid *Grid) Search(x int, y int, dir Direction, word []rune) int {
 	for _, char := range word {
 		// Ensure we're within the grid bounds
 		if x < 0 || y < 0 || y > grid.Rows || x > grid.Cols {
@@ -99,26 +151,6 @@ func (grid *Grid) RuneAt(x int, y int) rune {
 	}
 
 	return grid.Runes[idx]
-}
-
-func (grid *Grid) CountWord(word []rune) int {
-	var matches int
-
-	for x := range grid.Cols {
-		for y := range grid.Rows {
-			// We're returning a 0 or 1 for convenience
-			matches += grid.search(x, y, DirNorth, word)
-			matches += grid.search(x, y, DirNorthEast, word)
-			matches += grid.search(x, y, DirEast, word)
-			matches += grid.search(x, y, DirSouthEast, word)
-			matches += grid.search(x, y, DirSouth, word)
-			matches += grid.search(x, y, DirSouthWest, word)
-			matches += grid.search(x, y, DirWest, word)
-			matches += grid.search(x, y, DirNorthWest, word)
-		}
-	}
-
-	return matches
 }
 
 func NewGrid(input string) Grid {
--- 2024/day04/main_test.go
+++ 2024/day04/main_test.go
@@ -17,18 +17,20 @@ MAMMMXMMMM
 MXMXAXMASX`
 
 const RESULT_1 = 18
-const RESULT_2 = 1
+const RESULT_2 = 9
 
-func TestSolution(t *testing.T) {
-
-	p1, _ := Solve(SAMPLE)
+func TestPartOne(t *testing.T) {
+	p1 := partOne(SAMPLE)
 	fmt.Printf("Part one: %d\n", p1)
 	if p1 != RESULT_1 {
 		t.Errorf("Expected: %v but received: %v", RESULT_1, p1)
 	}
+}
 
-	// fmt.Printf("Part two %d\n", p2)
-	// if p2 != RESULT_2 {
-	// 	t.Errorf("Expected: %v but received: %v", RESULT_2, p2)
-	// }
+func TestPartTwo(t *testing.T) {
+	p2 := partTwo(SAMPLE)
+	fmt.Printf("Part two %d\n", p2)
+	if p2 != RESULT_2 {
+		t.Errorf("Expected: %v but received: %v", RESULT_2, p2)
+	}
 }
